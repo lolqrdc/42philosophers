faaa15481e9881556efd5accc0a1658368517c29
lolqrdc <lola-rdc@outlook.fr>
2025-05-08 16:30:14 +0200

    update: ajout des appels des fonctions init dans le main.

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: lolq <lolq@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 11:03:21 by lolq              #+#    #+#             */
-/*   Updated: 2025/05/08 14:27:19 by lolq             ###   ########.fr       */
+/*   Updated: 2025/05/08 16:07:24 by lolq             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,17 @@
 
 int main (int argc, char **av)
 {
+    t_program       program;
+    t_philo         philos[PHILO_MAX];
+    pthread_mutex_t forks[PHILO_MAX];
+
     if (argc != 5 && argc != 6)
         return (printf("Please provide enough arguments.\n"), FAIL);
-    if (!valid_arg(av))
+    if (valid_arg(av) != SUCCESS)
         return (FAIL);
+    init_program(&program, philos);
+    init_fork(forks, ft_atoi(av[1]));
+    init_philos(philos, &program, fork, av);
+    return (SUCCESS);
 }
+
