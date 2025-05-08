9efe17686b31a0ea8208b1883e82cee8e356e0f6
lolqrdc <lola-rdc@outlook.fr>
2025-05-08 16:28:46 +0200

    feat: initialisation des structures, ne pas oublier d'ajouter start_time et last_meal .

--- philo/src/init/init.c
+++ philo/src/init/init.c
@@ -6,9 +6,70 @@
 /*   By: lolq <lolq@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 14:22:52 by lolq              #+#    #+#             */
-/*   Updated: 2025/05/08 14:22:59 by lolq             ###   ########.fr       */
+/*   Updated: 2025/05/08 16:06:40 by lolq             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
+/* @brief : Init les inputs proposé par l'utilisateur, init les values d'un
+philo, init du programme en entier et init les fourchettes. */
+
+void    init_input(t_philo *philo, char **av)
+{
+    philo->nb_philos = ft_atoi(av[1]);
+    philo->time_to_die = ft_atoi(av[2]);
+    philo->time_to_eat = ft_atoi(av[3]);
+    philo->time_to_sleep = ft_atoi(av[4]);
+    if (av[5])
+        philo->nb_time_to_eat = ft_atoi(av[5]);
+    else
+        philo->nb_time_to_eat = -1;
+}
+
+void    init_philos(t_philo *philo, t_program *program, pthread_mutex_t *fork, char **av)
+{
+    int i;
+
+    i = 0;
+    while (i < ft_atoi(av[1]))
+    {
+        philo[i].id = i + 1;
+        philo[i].eating = 0;
+        philo[i].meals_eaten = 0;
+        init_input(&philo[i], av);
+        //philo[i].start_time =
+        //philo[i].last_meal = 
+        philo[i].write_lock = &program->write_lock;
+        philo[i].dead_lock = &program->dead_lock;
+        philo[i].meal_lock = &program->meal_lock;
+        philo[i].dead = &program->dead_flag;
+        philo[i].l_fork = &fork[i];
+        if (i == 0)
+            philo[i].r_fork = &fork[philo[i].nb_philos - 1];
+        else
+            philo[i].r_fork = &fork[i - 1];
+        i++;
+    }
+}
+
+void init_program(t_program *program, t_philo *philos)
+{
+    program->dead_flag = 0;
+    program->philos = philos;
+    pthread_mutex_init(&program->write_lock, NULL);
+    pthread_mutex_init(&program->meal_lock, NULL);
+    pthread_mutex_init(&program->dead_lock, NULL);
+}
+
+void    init_fork(pthread_mutex_t *forks, int nb_philos)
+{
+    int i;
+
+    i = 0;
+    while (i < nb_philos)
+    {
+        pthread_mutex_init(&forks[i], NULL);
+        i++;
+    }
+}
